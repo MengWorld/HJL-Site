f670c6adc1dd5a52435534b2e6e1dfa120bec6ef
CornWorld <43746030+CornWorld@users.noreply.github.com>
2024-04-07 12:08:01 +0800

    [Feat] Add host filed in vite config

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,9 @@ export default defineConfig({
     }
   },
   server: {
+    host: '0.0.0.0',
+    port: 5173,
+    https: false,
     proxy: {
       '/api': {
         target: 'http://localhost:3000',
